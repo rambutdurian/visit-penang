29b2f39dc21ee192d5dc343cf86818772eba8e0a
rambutdurian <rambutdurian@users.noreply.github.com>
2025-01-06 14:59:32 +0000

    deploy: 286e168450897d897fe49b8e92befd8b4af3ac23

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Accept connections from any network
+    port: 5173, // Optional: Specify the port you want to use
+  },
+});
